547db3a135f1bad2199273b32c63c1f422265923
Javier Burguete <jburguete@eead.csic.es>
2019-10-24 15:57:21 +0200

    Improving build script to work in BSD systems

--- 3.4.1/build.sh
+++ 3.4.1/build.sh
@@ -1,5 +1,11 @@
+if test -x `command -v gmake`; then
+	MAKE="gmake"
+else
+	MAKE="make"
+fi
+export MAKE
 aclocal
 autoconf
 automake --add-missing
 ./configure --with-freeglut --with-pgo
-make
+$MAKE
