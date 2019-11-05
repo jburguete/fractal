9fa2006b37c582c0e77e55411b6789af24c0cd5a
Javier Burguete <jburguete@eead.csic.es>
2019-11-05 16:22:49 +0100

    Improving building scripts

--- 3.4.1/build.sh
+++ 3.4.1/build.sh
@@ -1,11 +1,9 @@
-if test -x `command -v gmake`; then
-	MAKE="gmake"
-else
-	MAKE="make"
-fi
-export MAKE
 aclocal
 autoconf
 automake --add-missing
 ./configure --with-freeglut --with-pgo
-$MAKE
+if test -x "`command -v gmake`"; then 
+	gmake 
+else
+	make
+fi
